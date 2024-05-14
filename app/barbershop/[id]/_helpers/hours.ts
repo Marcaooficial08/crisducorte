39b5bfd50fcf8c9d353389e867d84e09bf919afe
Marcos <marcoscaru@hotmail.com>
2024-05-14 14:10:19 -0300

    verificacao horario volta

--- app/barbershop/[id]/_helpers/hours.ts
+++ app/barbershop/[id]/_helpers/hours.ts
@@ -1,7 +1,6 @@
-import { setHours, setMinutes, format, addMinutes, isAfter } from "date-fns";
+import { setHours, setMinutes, format, addMinutes } from "date-fns";
 
 export function generateDayTimeList(date: Date): string[] {
-  const now = new Date(); // Obtém a data e hora atual
   const startTime = setMinutes(setHours(date, 9), 0); // Define a hora inicial para 09:00
   const secondTime = setMinutes(setHours(date, 10), 30); // Define o segundo horário começando às 10:30
   const endTime = setMinutes(setHours(date, 19), 30); // Define a hora final para 19:30
@@ -10,14 +9,20 @@ export function generateDayTimeList(date: Date): string[] {
 
   let currentTime = startTime;
 
-  // Adiciona os horários futuros à lista
+  // Adiciona o primeiro horário
+  timeList.push(format(startTime, "HH:mm"));
+
+  // Adiciona o segundo horário
+  timeList.push(format(secondTime, "HH:mm"));
+
+  // Continua adicionando horários em intervalos de uma hora
+  currentTime = addMinutes(secondTime, interval);
   while (currentTime <= endTime) {
-    if (isAfter(currentTime, now)) {
-      timeList.push(format(currentTime, "HH:mm"));
-    }
+    timeList.push(format(currentTime, "HH:mm"));
     currentTime = addMinutes(currentTime, interval);
   }
 
   return timeList;
 }
+
 
